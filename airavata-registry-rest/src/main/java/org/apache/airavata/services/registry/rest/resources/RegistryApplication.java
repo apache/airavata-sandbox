7de5d0b0d189138c3b7725dd58349ca2138e20b4
Suresh Marru <smarru@apache.org>
2012-09-13 18:30:57 +0000

    accepting patch for AIRAVATA 561, Thanks Amila
    git-svn-id: https://svn.apache.org/repos/asf/incubator/airavata/sandbox@1384457 13f79535-47bb-0310-9956-ffa450edef68

--- airavata-registry-rest/src/main/java/org/apache/airavata/services/registry/rest/resources/RegistryApplication.java
+++ airavata-registry-rest/src/main/java/org/apache/airavata/services/registry/rest/resources/RegistryApplication.java
@@ -1,3 +1,24 @@
+package org.apache.airavata.services.registry.rest.resources;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RegistryApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+        // register root resource
+        classes.add(RegistryResource.class);
+        return classes;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return super.getSingletons();
+    }
+
+}
 /*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
--- airavata-registry-rest/src/main/webapp/index.jsp
+++ airavata-registry-rest/src/main/webapp/index.jsp
@@ -1,3 +1,11 @@
+<html>
+<body>
+<h2>Jersey RESTful Web Application!</h2>
+<p><a href="webresources/myresource">Jersey resource</a>
+<p>Visit the <a href="http://jersey.java.net">Project Jersey website</a>
+for more information on Jersey!
+</body>
+</html>
 <%--
   Licensed to the Apache Software Foundation (ASF) under one or more
   contributor license agreements.  See the NOTICE file distributed with
