21034238558ec1da464c12a544e718ba642b79d1
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2012-10-30 19:06:51 +0000

    Adding helper methods.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/sandbox@1403814 13f79535-47bb-0310-9956-ffa450edef68

--- airavata-registry-rest/src/main/java/org/apache/airavata/services/registry/rest/utils/DescriptorUtil.java
+++ airavata-registry-rest/src/main/java/org/apache/airavata/services/registry/rest/utils/DescriptorUtil.java
@@ -1,7 +1,16 @@
 package org.apache.airavata.services.registry.rest.utils;
 
+import org.apache.airavata.client.api.AiravataAPI;
+import org.apache.airavata.client.api.AiravataAPIInvocationException;
+import org.apache.airavata.client.api.ApplicationManager;
+import org.apache.airavata.commons.gfac.type.ApplicationDeploymentDescription;
 import org.apache.airavata.commons.gfac.type.HostDescription;
-import org.apache.airavata.schemas.gfac.GlobusHostType;
+import org.apache.airavata.commons.gfac.type.ServiceDescription;
+import org.apache.airavata.schemas.gfac.*;
+
+import javax.ws.rs.FormParam;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DescriptorUtil {
 
@@ -21,5 +30,54 @@ public class DescriptorUtil {
             host.getType().setHostAddress(hostAddress);
         }
         return host;
+    }
+
+    public static ApplicationDeploymentDescription registerApplication(String appName, String exeuctableLocation, String scratchWorkingDirectory, String hostName,
+                                                                       String projAccNumber, String queueName, String cpuCount, String nodeCount, String maxMemory) throws Exception {
+        // Create Application Description
+        ApplicationDeploymentDescription appDesc = new ApplicationDeploymentDescription(GramApplicationDeploymentType.type);
+        GramApplicationDeploymentType app = (GramApplicationDeploymentType) appDesc.getType();
+        app.setCpuCount(Integer.parseInt(cpuCount));
+        app.setNodeCount(Integer.parseInt(nodeCount));
+        ApplicationDeploymentDescriptionType.ApplicationName name = appDesc.getType().addNewApplicationName();
+        name.setStringValue(appName);
+        app.setExecutableLocation(exeuctableLocation);
+        app.setScratchWorkingDirectory(scratchWorkingDirectory);
+        ProjectAccountType projectAccountType = ((GramApplicationDeploymentType) appDesc.getType()).addNewProjectAccount();
+        projectAccountType.setProjectAccountNumber(projAccNumber);
+        QueueType queueType = app.addNewQueue();
+        queueType.setQueueName(queueName);
+        app.setMaxMemory(Integer.parseInt(maxMemory));
+        return appDesc;
+    }
+
+    public static ServiceDescription getServiceDescription(String serviceName, String inputName, String inputType,
+                                                           String outputName, String outputType) {
+        // Create Service Description
+        ServiceDescription serv = new ServiceDescription();
+        serv.getType().setName(serviceName);
+
+        InputParameterType input = InputParameterType.Factory.newInstance();
+        input.setParameterName(inputName);
+        ParameterType parameterType = input.addNewParameterType();
+        parameterType.setType(DataType.Enum.forString(inputType));
+        parameterType.setName(inputName);
+        List<InputParameterType> inputList = new ArrayList<InputParameterType>();
+        inputList.add(input);
+        InputParameterType[] inputParamList = inputList.toArray(new InputParameterType[inputList
+                .size()]);
+
+        OutputParameterType output = OutputParameterType.Factory.newInstance();
+        output.setParameterName(outputName);
+        ParameterType parameterType1 = output.addNewParameterType();
+        parameterType1.setType(DataType.Enum.forString(outputType));
+        parameterType1.setName(outputName);
+        List<OutputParameterType> outputList = new ArrayList<OutputParameterType>();
+        outputList.add(output);
+        OutputParameterType[] outputParamList = outputList
+                .toArray(new OutputParameterType[outputList.size()]);
+        serv.getType().setInputParametersArray(inputParamList);
+        serv.getType().setOutputParametersArray(outputParamList);
+        return serv;
     }
 }
