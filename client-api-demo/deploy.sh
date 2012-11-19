fe31537fc101e04709af7b507968ad4ab91d924d
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2012-11-19 19:56:29 +0000

    Updates to scripts.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/sandbox@1411376 13f79535-47bb-0310-9956-ffa450edef68

--- client-api-demo/deploy.sh
+++ client-api-demo/deploy.sh
@@ -12,10 +12,12 @@ rm -r -f /home/heshan/Dev/sc12/demo1/apache-tomcat-7.0.28/webapps/client-api-dem
 echo "Copying the client-api-demo war to webapps"
 cp target/client-api-demo.war /home/heshan/Dev/sc12/demo1/apache-tomcat-7.0.28/webapps
 
+sleep 15
+
 echo "Copying mysql jar to airavata-rest-services module"
 cp ~/Downloads/mysql-connector-java-5.0.8/mysql-connector-java-5.0.8-bin.jar ~/Dev/sc12/demo1/apache-tomcat-7.0.28/webapps/airavata-rest-services/WEB-INF/lib/
 
-sleep 10
+#sleep 10
 #echo "Copying the repository.properties to webapp ..."
 #cp /home/heshan/Dev/sc12/demo1/repository.properties /home/heshan/Dev/sc12/demo1/apache-tomcat-7.0.28/webapps/client-api-demo/WEB-INF/classes/ -v
 #echo "Copying the deployment.properties to webapp ..."
