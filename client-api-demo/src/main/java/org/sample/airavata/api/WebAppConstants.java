a06751188cb467e627be6212b4698a94e67cc6f0
Heshan Theekshana Suriyaarachchi <heshan@apache.org>
2012-11-20 19:52:43 +0000

    Removing the jsps and the associated bean/helper classes. These are removed because we longer take jsp approach towards gadgets.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/sandbox@1411824 13f79535-47bb-0310-9956-ffa450edef68

--- client-api-demo/src/main/java/org/sample/airavata/api/WebAppConstants.java
+++
@@ -1,6 +0,0 @@
-package org.sample.airavata.api;
-
-public class WebAppConstants {
-    public static final String CA_CERT_PATH = "ca.certificates.directory";
-    public static final String HOST_NAME = "host.name";
-}
