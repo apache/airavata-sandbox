01cb55128790d661efa6f2b3cf76eab3107c920c
Lahiru Gunathilake <lahiru@apache.org>
2013-01-21 16:40:26 +0000

    committing patch given by Shabaz for https://issues.apache.org/jira/browse/AIRAVATA-657.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/sandbox@1436473 13f79535-47bb-0310-9956-ffa450edef68

--- grid-tools/bes-client/src/main/java/org/apache/airavata/sample/bes/CreateAndTerminateActivityTask.java
+++ grid-tools/bes-client/src/main/java/org/apache/airavata/sample/bes/CreateAndTerminateActivityTask.java
@@ -77,3 +77,82 @@ public class CreateAndTerminateActivityTask {
 	
 
 }
+package org.apache.airavata.sample.bes;
+
+import java.util.Calendar;
+
+import org.ggf.schemas.bes.x2006.x08.besFactory.ActivityStateEnumeration;
+import org.ggf.schemas.bes.x2006.x08.besFactory.CreateActivityDocument;
+import org.ggf.schemas.bes.x2006.x08.besFactory.CreateActivityResponseDocument;
+import org.ggf.schemas.bes.x2006.x08.besFactory.TerminateActivitiesResponseType;
+import org.w3.x2005.x08.addressing.EndpointReferenceType;
+
+import de.fzj.unicore.bes.client.FactoryClient;
+import de.fzj.unicore.wsrflite.xmlbeans.WSUtilities;
+import eu.unicore.security.util.client.IClientProperties;
+
+public class CreateAndTerminateActivityTask {
+
+	private BESJob job;
+
+	private IClientProperties sec;
+	private String jobId;
+	
+	
+	public CreateAndTerminateActivityTask(BESJob job, IClientProperties sec) {
+		this.job = job;
+		this.sec = sec;
+		
+	}
+
+	public void startJob() throws Exception {
+		String factoryUrl = job.getFactoryUrl();
+		EndpointReferenceType eprt = EndpointReferenceType.Factory
+				.newInstance();
+		eprt.addNewAddress().setStringValue(factoryUrl);
+		System.out.println("========================================");
+		System.out.println(String.format("Job Submitted to %s.\n", factoryUrl));
+		FactoryClient factory = new FactoryClient(eprt, sec);
+		CreateActivityDocument cad = CreateActivityDocument.Factory
+				.newInstance();
+		cad.addNewCreateActivity().addNewActivityDocument()
+				.setJobDefinition(job.getJobDoc().getJobDefinition());
+		CreateActivityResponseDocument response = factory.createActivity(cad);
+		EndpointReferenceType activityEpr = response
+				.getCreateActivityResponse().getActivityIdentifier();
+		jobId = WSUtilities.extractResourceID(activityEpr);
+		if (jobId == null) {
+			jobId = new Long(Calendar.getInstance().getTimeInMillis())
+					.toString();
+		}
+		
+		String status;
+		
+		status = String.format("Job %s is %s.\n", activityEpr.getAddress()
+				.getStringValue(), factory.getActivityStatus(activityEpr)
+				.toString()).toString();
+		
+		System.out.println(status);
+		
+		TerminateActivitiesResponseType terminateResType = factory.terminateActivity(activityEpr);
+		
+		Thread.sleep(500);
+		System.out.println(terminateResType);
+
+		if (!(factory.getActivityStatus(activityEpr) == ActivityStateEnumeration.CANCELLED)) {
+			throw new Exception("Job "+activityEpr.getAddress().getStringValue()+" not CANCELLED");
+		}
+		
+		status = String.format("Job %s is %s.\n", activityEpr.getAddress()
+				.getStringValue(), factory.getActivityStatus(activityEpr)
+				.toString()).toString();
+		
+		System.out.println(status);
+		
+
+		
+	}
+
+	
+
+}
--- grid-tools/bes-client/src/main/java/org/apache/airavata/sample/bes/RunAndTerminateJob.java
+++ grid-tools/bes-client/src/main/java/org/apache/airavata/sample/bes/RunAndTerminateJob.java
@@ -34,3 +34,39 @@ public class RunAndTerminateJob extends AbstractJobCommand {
 
 }
 
+package org.apache.airavata.sample.bes;
+
+import java.io.File;
+
+import org.ggf.schemas.jsdl.x2005.x11.jsdl.JobDefinitionDocument;
+
+public class RunAndTerminateJob extends AbstractJobCommand {
+
+	public RunAndTerminateJob() {
+		super();
+	}
+
+	public void runAndTerminate(){
+		JobDefinitionDocument jobDoc = null;
+		try {
+			jobDoc = JobDefinitionDocument.Factory.parse(new File(dateJsdlPath));
+		} catch (Exception e) {
+			System.err.println("Error parsing JSDL instance. " + e);
+		}
+
+		BESJob job = new BESJob();
+		job.setFactory(factoryUrl);
+		job.setJobDoc(jobDoc);
+
+		CreateAndTerminateActivityTask th = new CreateAndTerminateActivityTask(job, securityProperties);
+		try {
+			th.startJob();
+		} catch (Exception e) {
+
+			System.err.println("Couldn't run job: " + e);
+		}
+	}
+	
+
+}
+
