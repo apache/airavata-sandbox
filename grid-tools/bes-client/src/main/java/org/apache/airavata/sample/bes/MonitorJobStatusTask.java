5446b53cc3f497701c7208aeeff40a7a6a0d4dd7
Lahiru Gunathilake <lahiru@apache.org>
2013-01-22 15:39:23 +0000

    fixing double commit in bes-client.
    git-svn-id: https://svn.apache.org/repos/asf/airavata/sandbox@1436999 13f79535-47bb-0310-9956-ffa450edef68

--- grid-tools/bes-client/src/main/java/org/apache/airavata/sample/bes/MonitorJobStatusTask.java
+++ grid-tools/bes-client/src/main/java/org/apache/airavata/sample/bes/MonitorJobStatusTask.java
@@ -17,36 +17,4 @@ public class MonitorJobStatusTask {
 		this.jobEPR = jobEPR;
 		this.sec = sec; 
 	}
-	
-	
-	
-	
-
-	
-}
-package org.apache.airavata.sample.bes;
-
-import org.w3.x2005.x08.addressing.EndpointReferenceType;
-
-import de.fzj.unicore.wsrflite.xmlbeans.WSUtilities;
-
-import eu.unicore.security.util.client.IClientProperties;
-
-public class MonitorJobStatusTask {
-	
-	private String factoryUrl;
-	private EndpointReferenceType jobEPR;
-	private IClientProperties sec;
-	
-	public MonitorJobStatusTask(String factoryUrl, IClientProperties sec, EndpointReferenceType jobEPR) {
-		this.factoryUrl = factoryUrl;
-		this.jobEPR = jobEPR;
-		this.sec = sec; 
-	}
-	
-	
-	
-	
-
-	
 }
