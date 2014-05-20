d43e5037c566a7d1acf58d1ff6c3a0fd3c23130c
Lahiru Gunathilake <lahiru@apache.org>
2014-05-20 17:58:28 +0000

    fixing further issue
    git-svn-id: https://svn.apache.org/repos/asf/airavata/sandbox@1596340 13f79535-47bb-0310-9956-ffa450edef68

--- gfac-sample/local-handler-sample/src/main/java/org/apache/airavata/gfac/local/handler/InputEmailHandler.java
+++ gfac-sample/local-handler-sample/src/main/java/org/apache/airavata/gfac/local/handler/InputEmailHandler.java
@@ -20,24 +20,84 @@
 */
 package org.apache.airavata.gfac.local.handler;
 
+import org.apache.airavata.commons.gfac.type.ActualParameter;
 import org.apache.airavata.gfac.core.context.JobExecutionContext;
 import org.apache.airavata.gfac.core.handler.GFacHandler;
 import org.apache.airavata.gfac.core.handler.GFacHandlerException;
-import javax.mail.*;
+import org.apache.airavata.schemas.gfac.StringParameterType;
+
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
 import java.lang.String;
 import java.util.Map;
 import java.util.Properties;
+
+
 
 public class InputEmailHandler implements GFacHandler {
     private Properties props;
 
     public void initProperties(Properties properties) throws GFacHandlerException {
+        System.out.println("Initializing " + InputEmailHandler.class + " Handler...");
         props = properties;
     }
 
     public void invoke(JobExecutionContext jobExecutionContext) throws GFacHandlerException {
+//        Session session = Session.getInstance(props,
+//                new javax.mail.Authenticator() {
+//                    protected PasswordAuthentication getPasswordAuthentication() {
+//                        return new PasswordAuthentication((String) props.get("username"), (String) props.get("password"));
+//                    }
+//                });
+//
+//        Message message = new MimeMessage(session);
+//        try {
+//            message.setFrom(new InternetAddress((String) props.get("username")));
+//            message.setRecipients(Message.RecipientType.TO,
+//                    InternetAddress.parse((String) props.get("username")));
+//            message.setSubject("GFAC Input Email");
+//
+
+//            Transport.send(message);
+//        } catch (MessagingException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
+//        System.out.println("Start Emailing the input values ... ");
+//        try {
+//            Session mailSession = Session.getDefaultInstance(props, null);
+//            Transport transport = mailSession.getTransport();
+//
+//            MimeMessage message = new MimeMessage(mailSession);
+//            message.setSubject("GFAC Inputs");
+//            Map<String, Object> parameters = jobExecutionContext.getOutMessageContext().getParameters();
+//            StringBuffer buffer = new StringBuffer();
+//            for (String input : parameters.keySet()) {
+//                ActualParameter inParam = (ActualParameter) parameters.get(input);
+//                String paramDataType = inParam.getType().getType().toString();
+//                if ("String".equals(paramDataType)) {
+//                    String stringPrm = ((StringParameterType) inParam.getType())
+//                            .getValue();
+//                    buffer.append("Input Name: input: Input Value: " + stringPrm + "\n");
+//                }
+//            }
+//            message.setContent(buffer.toString(), "text/plain");
+//            message.addRecipient(Message.RecipientType.TO,
+//                    new InternetAddress((String) props.get("username")));
+//
+//            transport.connect();
+//            transport.sendMessage(message,
+//                    message.getRecipients(Message.RecipientType.TO));
+//            transport.close();
+//            System.out.println("Sent email to : " + props.get("username"));
+//        } catch (Exception e) {
+//
+//        }
+             System.out.println("Start Emailing the input values ... ");
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
@@ -55,9 +115,17 @@ public class InputEmailHandler implements GFacHandler {
             Map<String, Object> parameters = jobExecutionContext.getInMessageContext().getParameters();
             StringBuffer buffer = new StringBuffer();
             for (String input : parameters.keySet()) {
-                buffer.append("Input Name: input: Input Value: " + parameters.get(input) + "\n");
+                ActualParameter inParam = (ActualParameter) parameters.get(input);
+                String paramDataType = inParam.getType().getType().toString();
+                if ("String".equals(paramDataType)) {
+                    String stringPrm = ((StringParameterType) inParam.getType())
+                            .getValue();
+                    buffer.append("Input Name: input: Input Value: " + stringPrm + "\n");
+                }
             }
+            message.setText(buffer.toString());
             Transport.send(message);
+            System.out.println("Sent email to : " + props.get("username"));
         } catch (MessagingException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
--- gfac-sample/local-handler-sample/src/main/java/org/apache/airavata/gfac/local/handler/OutputEmailHandler.java
+++ gfac-sample/local-handler-sample/src/main/java/org/apache/airavata/gfac/local/handler/OutputEmailHandler.java
@@ -20,9 +20,11 @@
 */
 package org.apache.airavata.gfac.local.handler;
 
+import org.apache.airavata.commons.gfac.type.ActualParameter;
 import org.apache.airavata.gfac.core.context.JobExecutionContext;
 import org.apache.airavata.gfac.core.handler.GFacHandler;
 import org.apache.airavata.gfac.core.handler.GFacHandlerException;
+import org.apache.airavata.schemas.gfac.StringParameterType;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -34,10 +36,12 @@ public class OutputEmailHandler implements GFacHandler {
     private Properties props;
 
     public void initProperties(Properties properties) throws GFacHandlerException {
+        System.out.println("Initializing " + OutputEmailHandler.class + " Handler...");
         props = properties;
     }
 
     public void invoke(JobExecutionContext jobExecutionContext) throws GFacHandlerException {
+        System.out.println("Start Emailing the output values ... ");
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
@@ -50,15 +54,22 @@ public class OutputEmailHandler implements GFacHandler {
             message.setFrom(new InternetAddress((String) props.get("username")));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse((String) props.get("username")));
-            message.setSubject("GFAC Input Email");
+            message.setSubject("GFAC Output Email");
 
             Map<String, Object> parameters = jobExecutionContext.getOutMessageContext().getParameters();
             StringBuffer buffer = new StringBuffer();
             for (String input : parameters.keySet()) {
-                buffer.append("Input Name: input: Input Value: " + parameters.get(input) + "\n");
+                ActualParameter inParam = (ActualParameter) parameters.get(input);
+                String paramDataType = inParam.getType().getType().toString();
+                if ("String".equals(paramDataType)) {
+                    String stringPrm = ((StringParameterType) inParam.getType())
+                            .getValue();
+                    buffer.append("Output Name: " + input + " Output Value: " + stringPrm + "\n");
+                }
             }
             message.setText(buffer.toString());
             Transport.send(message);
+            System.out.println("Sent email to : " + props.get("username"));
         } catch (MessagingException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
