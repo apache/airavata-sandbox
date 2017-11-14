9faa0175d18b1962283cd7800d8baa7c5e1dce08
nikchett <nikchett@iu.edu>
2017-11-13 23:11:53 -0500

    Corrected package name

--- allocation-manager/airavata-allocation-manager/airavata-allocation-manager-server/src/main/java/org/apache/airavata/allocation/manager/server/AllocationManagerServerHandler.java
+++ allocation-manager/airavata-allocation-manager/airavata-allocation-manager-server/src/main/java/org/apache/airavata/allocation/manager/server/AllocationManagerServerHandler.java
@@ -17,7 +17,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.airavata.sharing.registry.server;
+package org.apache.airavata.allocation.manager.server;
 
 import org.apache.airavata.allocation.manager.models.*;
 
