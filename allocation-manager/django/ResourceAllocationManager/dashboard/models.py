b8ffad7758dc3077a888b5b414f71921e446a561
Prasandeep Singh <prasan.ubhi@gmail.com>
2017-12-01 03:23:24 -0500

    updated Request Model, Added forms for Create Request

--- allocation-manager/django/ResourceAllocationManager/dashboard/models.py
+++ allocation-manager/django/ResourceAllocationManager/dashboard/models.py
@@ -2,19 +2,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.urlresolvers import reverse
-
-
+#from datetime import date
 
 # Create your models here.
 class Request(models.Model):
     request_title = models.CharField(max_length=250)
     request_description = models.CharField(max_length=500)
     request_status = models.CharField(max_length=20)
-    request_comments = models.FileField()
     allocation_type = models.CharField(max_length=100)
-    cpu_hours_requested = models.CharField(max_length=10)
-    cpu_hours_allocated = models.CharField(max_length=10)
-
+    applications_to_be_used = models.CharField(max_length=100)
+    disk_usage_range_per_job = models.BigIntegerField()
+    document = models.FileField()
+    field_of_science = models.CharField(max_length=50)
+    keywords = models.CharField(max_length=100)
+    max_memory_per_cpu = models.BigIntegerField()
+    num_cpus_per_job = models.BigIntegerField()
+    request_reviewed_and_funded_by = models.CharField(max_length=100)
+    request_date = models.DateTimeField(blank=True, null=True)
+    service_units = models.BigIntegerField()
+    specific_resource_selection = models.CharField(max_length=100)
+    typical_su_per_job = models.BigIntegerField()
 
     def get_absolute_url(self):
         return reverse('dashboard/detail.html', kwargs={'pk': self.pk})
