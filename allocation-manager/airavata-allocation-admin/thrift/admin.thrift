e34b30ae3bcd6db9cafb2e4e27982261be6f2ca5
nikchett <nikchett@iu.edu>
2017-12-03 01:12:44 -0500

    Removing redudant dependies

--- allocation-manager/airavata-allocation-admin/thrift/admin.thrift
+++
@@ -1,9 +0,0 @@
-namespace org.apache.airavata.allocation.manager.admin;
-      
-      
-    service ManageReview {  
-            void addReviewers(1:string projectId, 2: required list<string> reviewers), 
-
-            void removeReviewer(1:string projectId, 2:string reviewer)
-
-    }
