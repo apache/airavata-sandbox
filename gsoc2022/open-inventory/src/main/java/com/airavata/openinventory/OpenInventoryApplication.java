63faa3d9ed665395ed7b70e82fe18e3c987218e1
Bhavesh Asanabada <asanabhavesh@gmail.com>
2022-05-14 08:10:37 +0530

    initialized spring-boot

---
+++ gsoc2022/open-inventory/src/main/java/com/airavata/openinventory/OpenInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.airavata.openinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenInventoryApplication.class, args);
+	}
+
+}
