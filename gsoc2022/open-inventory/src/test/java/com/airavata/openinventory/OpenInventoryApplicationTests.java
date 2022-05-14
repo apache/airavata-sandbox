8ed3458f55e2ae7b266acee41b18885d188df01e
Bhavesh Asanabada <62237873+bhavesh-asana@users.noreply.github.com>
2022-05-14 07:03:40 +0530

    Initialized spring-boot

---
+++ gsoc2022/open-inventory/src/test/java/com/airavata/openinventory/OpenInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airavata.openinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
