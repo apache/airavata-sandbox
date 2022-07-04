408a2bab9c5e4b7624a3e212f2ad94664e6ab0e8
Bhavesh Asanabada <asanabhavesh@gmail.com>
2022-07-04 06:12:12 +0530

    vue webpage

---
+++ gsoc2022/smilesdb/smiles_dashboard/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <CalcInfo />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+//
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+
+import CalcInfo from './components/CalcInfo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalcInfo,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
