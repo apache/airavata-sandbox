2f670bc72b53d4ba69ebf55123b9a707aaa289d3
BECOB257_Aishwarya Sinhasane <avsinhas@iu.edu>
2022-09-08 17:32:58 -0400

    Molecule Editor and Avogadro integration

--- gsoc2022/seagrid-rich-client/renderer.js
+++ gsoc2022/seagrid-rich-client/renderer.js
@@ -1,1 +1,20 @@
+let spawn = require("child_process").spawn;
 
+let bat = spawn("cmd.exe", [
+    "/c",          // Argument for cmd.exe to carry out the specified script
+    "test.bat", // Path to your file
+    //"argument1",   // First argument
+    //"argumentN"    // n-th argument
+]);
+
+bat.stdout.on("data", (data) => {
+    // Handle data...
+});
+
+bat.stderr.on("data", (err) => {
+    // Handle error...
+});
+
+bat.on("exit", (code) => {
+    // Handle exit
+});
