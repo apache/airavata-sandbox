8df97dd8d0f1a1e9c90078550569bc681de076fb
Bhavesh Asanabada <bhavesh@Bhavesh-MacBook.local>
2022-09-12 13:43:11 +0530

    modified files

--- gsoc2022/smilesdb/Server/src/main/java/com/smiles/molData/MoleculeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.smiles.molData;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MoleculeRepo extends MongoRepository<MoleculeEntity, String> {
-
-    MoleculeEntity findBySmiles(String smiles);
-}
